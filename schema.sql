fd4e8456487fd63535dd03d8b4f0ce86496ad6e8
David Schneider <david.schneider@bivab.de>
2015-04-14 17:10:27 +0200

    Updated schema

--- schema.sql
+++ schema.sql
@@ -1,20 +1,68 @@
-CREATE TABLE "sessions" (
+---
+CREATE TABLE "info" (
+  "key" TEXT NOT NULL,
+  "value" TEXT DEFAULT "" NOT NULL
+);
+CREATE UNIQUE INDEX info_key ON info(key);
+INSERT INTO info (key, value) VALUES ("schema_version", "2.0");
+
+---
+CREATE TABLE "focus_areas" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "slot" TEXT NOT NULL,
-  "rhythm" INTEGER DEFAULT 0 NOT NULL,
-  "duration" INTEGER DEFAULT 2 NOT NULL,
+  "name" TEXT NOT NULL,
   "created_at" TEXT NOT NULL,
   "updated_at" TEXT NOT NULL
 );
 
+CREATE TABLE "courses" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "name" TEXT NOT NULL,
+  "long_name" TEXT,
+  "PO" TEXT,
+  "created_at" TEXT NOT NULL,
+  "updated_at" TEXT NOT NULL
+);
+CREATE UNIQUE INDEX course_name ON courses(name);
+
+CREATE TABLE "departments" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "name" TEXT NOT NULL,
+  "long_name" TEXT NOT NULL,
+  "created_at" TEXT NOT NULL,
+  "updated_at" TEXT NOT NULL
+);
+CREATE UNIQUE INDEX department_name ON departments(name);
+
+CREATE TABLE "modules" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "name" TEXT NOT NULL,
+  "frequency" TEXT,
+  "created_at" TEXT NOT NULL,
+  "updated_at" TEXT NOT NULL
+);
+CREATE UNIQUE INDEX module_name ON modules(name);
+
+
+---
+CREATE TABLE "sessions" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "group_id" INTEGER NOT NULL,
+  "slot" TEXT NOT NULL,
+  "rhythm" INTEGER DEFAULT 0 NOT NULL,
+  "duration" INTEGER DEFAULT 2 NOT NULL,
+  "created_at" TEXT NOT NULL,
+  "updated_at" TEXT NOT NULL,
+  FOREIGN KEY(group_id) REFERENCES groups(id)
+);
+
 CREATE TABLE "units" (
-  "id" integer primary key autoincrement,
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "title" text not null,
-  "department" integer not null,
+  "department_id" INTEGER not null,
   "duration" INTEGER DEFAULT 0 not null,
   "created_at" text not null,
   "updated_at" text not null,
-  FOREIGN KEY(department) REFERENCES departments(name)
+  FOREIGN KEY(department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE "groups" (
@@ -26,68 +74,44 @@ CREATE TABLE "groups" (
   FOREIGN KEY(unit_id) REFERENCES units(id)
 );
 
-CREATE TABLE "group_sessions" (
-  "group_id" INTEGER NOT NULL,
-  "session_id" INTEGER NOT NULL,
-  FOREIGN KEY(group_id) REFERENCES groups(id),
-  FOREIGN KEY(session_id) REFERENCES sessions(id)
-);
 
-CREATE TABLE "mapping" (
-  "module" TEXT NOT NULL,
-  "class" INTEGER DEFAULT 1 NOT NULL,
-  "course" TEXT NOT NULL,
-  "type" TEXT DEFAULT "P" NOT NULL,
-  "focus_area_id" INTEGER DEFAULT 0 NOT NULL,
-  "semester" INTEGER NOT NULL,
-  "unit_id" INTEGER NOT NULL,
-  FOREIGN KEY(module) REFERENCES modules(name),
-  FOREIGN KEY(course) REFERENCES courses(name),
-  FOREIGN KEY(unit_id) REFERENCES units(id),
+---
+
+CREATE TABLE "modules_focus_areas" (
+  "module_id" INTEGER NOT NULL,
+  "focus_area_id" INTEGER NOT NULL,
+  FOREIGN KEY(module_id) REFERENCES modules(id),
   FOREIGN KEY(focus_area_id) REFERENCES focus_areas(id)
 );
 
-CREATE TABLE "modules" (
-  "name" TEXT PRIMARY KEY NOT NULL,
-  "frequency" TEXT,
-  "created_at" TEXT NOT NULL,
-  "updated_at" TEXT NOT NULL
-);
-CREATE UNIQUE INDEX module_name ON modules(name);
-
-CREATE TABLE "focus_areas" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "name" TEXT NOT NULL,
-  "created_at" TEXT NOT NULL,
-  "updated_at" TEXT NOT NULL
+CREATE TABLE "courses_modules" (
+  "course_id" INTEGER NOT NULL,
+  "module_id" INTEGER NOT NULL,
+  "type" TEXT NOT NULL, --- e (elective) or m (mandatory)
+  FOREIGN KEY(course_id) REFERENCES courses(id),
+  FOREIGN KEY(module_id) REFERENCES modules(id)
 );
 
-CREATE TABLE "courses" (
-  "name" TEXT NOT NULL PRIMARY KEY,
-  "long_name" TEXT,
-  "created_at" TEXT NOT NULL,
-  "updated_at" TEXT NOT NULL
+CREATE TABLE "courses_number_of_elective_modules" (
+  "course_id" INTEGER NOT NULL,
+  "amount" INTEGER NOT NULL DEFAULT "3",
+  FOREIGN KEY(course_id) REFERENCES courses(id)
 );
-CREATE UNIQUE INDEX course_name ON courses(name);
 
-CREATE TABLE "departments" (
-  "name" TEXT NOT NULL PRIMARY KEY,
-  "long_name" TEXT NOT NULL,
-  "created_at" TEXT NOT NULL,
-  "updated_at" TEXT NOT NULL
+CREATE TABLE "courses_modules_number_of_elective_units" (
+  "course_id" INTEGER NOT NULL,
+  "module_id" INTEGER NOT NULL,
+  "amount" INTEGER NOT NULL DEFAULT "3",
+  FOREIGN KEY(course_id) REFERENCES courses(id),
+  FOREIGN KEY(module_id) REFERENCES modules(id)
 );
-CREATE UNIQUE INDEX department_name ON departments(name);
 
-
-CREATE TABLE "info" (
-  "key" TEXT NOT NULL,
-  "value" TEXT DEFAULT "" NOT NULL
-);
-CREATE UNIQUE INDEX info_key ON info(key);
-INSERT INTO info (key, value) VALUES ("schema_version", "1.1");
-
-CREATE TABLE major_module_requirements (
-  course TEXT NOT NULL,
-  requirement INTEGER NOT NULL,
-  FOREIGN KEY(course) REFERENCES courses(name)
+CREATE TABLE "courses_modules_units" (
+  "course_id" INTEGER NOT NULL,
+  "module_id" INTEGER NOT NULL,
+  "unit_id" INTEGER NOT NULL,
+  "type" TEXT NOT NULL, --- e (elective) or m (mandatory)
+  FOREIGN KEY(course_id) REFERENCES courses(id),
+  FOREIGN KEY(module_id) REFERENCES modules(id),
+  FOREIGN KEY(unit_id) REFERENCES units(id)
 );
